f0640480f9c4fb2f8bfebc2b910e090d481c491e
Mehmet Ali <mali.caferx@gmail.com>
2020-06-16 16:45:44 +0900

    deisgn login page with ant design

--- frontend/src/components/Login/index.vue
+++ frontend/src/components/Login/index.vue
@@ -1,16 +1,70 @@
 <template>
-  <div>
-    Login
+  <div class="form">
+    <a-form :form="form" @submit="handleSubmit">
+      <a-form-item>
+        <a-input
+          placeholder="Email"
+          v-decorator="[
+            'Email',
+            {
+              rules: [
+                { required: true, message: '' },
+                { type: 'email', message: 'Please enter a valid email' },
+              ],
+            },
+          ]"
+        >
+          <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.55)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          type="password"
+          placeholder="Password"
+          v-decorator="[
+            'Password',
+            {
+              rules: [{ required: true, message: '' }],
+            },
+          ]"
+        >
+          <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.55)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit">
+          Login
+        </a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
-}
+  name: "Login",
+  data() {
+    return {
+      form: this.$form.createForm(this, { name: "coordinated" }),
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+        }
+      });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.form {
+  margin: 10% auto;
+  width: 35%;
+}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
+import Antd from 'ant-design-vue';
 import userRoutes from "./routes/userRoutes";
+import 'ant-design-vue/dist/antd.css';
 
+Vue.use(Antd);
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
-console.log(userRoutes)
 new Vue({
   router:userRoutes,
   render: (h) => h(App),
